cfe46d68dbfbc6fcf46dfb737a4b5ce689916ef3
John-John Tedro <udoprog@tedro.se>
2017-03-10 17:14:04 +0100

    [metric/filesystem] memory issue: force munmap through DirectBuffer#cleaner()

--- metric/filesystem/src/main/java/com/spotify/heroic/metric/filesystem/Segment.java
+++ metric/filesystem/src/main/java/com/spotify/heroic/metric/filesystem/Segment.java
@@ -4,7 +4,6 @@ import com.spotify.heroic.function.ThrowingBiFunction;
 import com.spotify.heroic.metric.filesystem.io.SegmentIterator;
 import com.spotify.heroic.metric.filesystem.segmentio.SegmentEncoding;
 import com.spotify.heroic.metric.filesystem.wal.Wal;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -13,8 +12,11 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.EnumSet;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import sun.nio.ch.DirectBuffer;
 
 @Data
+@Slf4j
 public class Segment<T extends Comparable<T>> {
     private final SegmentEncoding segmentEncoding;
     private final Path segmentPath;
@@ -25,37 +27,48 @@ public class Segment<T extends Comparable<T>> {
     private final Object lock = new Object();
 
     private volatile FileChannel file = null;
-    private volatile int fileRefCount = 0;
+    private volatile MappedByteBuffer mapped = null;
+    private volatile int refCount = 0;
 
     public SegmentIterator<T> open() throws Exception {
+        /* guarantee a valid mapped location after open */
         synchronized (lock) {
-            if (fileRefCount == 0) {
+            if (refCount++ == 0) {
                 file = (FileChannel) Files.newByteChannel(segmentPath,
                     EnumSet.of(StandardOpenOption.READ));
-            }
 
-            fileRefCount++;
+                final MappedByteBuffer opened =
+                    file.map(FileChannel.MapMode.READ_ONLY, 0, file.size());
+                opened.position((int) headerSize);
+                mapped = opened;
+            }
         }
 
-        return reader.apply(segmentEncoding, openByteBuffer()).closing(() -> {
-            synchronized (lock) {
-                final int count = --fileRefCount;
+        return reader.apply(segmentEncoding, mapped.asReadOnlyBuffer()).closing(() -> {
+            final FileChannel file;
+            final MappedByteBuffer mapped;
 
-                if (count == 0) {
-                    Segment.this.file.close();
-                    Segment.this.file = null;
+            synchronized (lock) {
+                if (--refCount != 0) {
+                    return;
                 }
+
+                file = this.file;
+                mapped = this.mapped;
+
+                this.file = null;
+                this.mapped = null;
             }
+
+            if (mapped instanceof DirectBuffer) {
+                ((DirectBuffer) mapped).cleaner().clean();
+            }
+
+            file.close();
         });
     }
 
     public boolean isNewer(final long txId) {
         return txId == Wal.DISABLED_TXID || header.getTxId() >= txId;
-    }
-
-    private MappedByteBuffer openByteBuffer() throws IOException {
-        final MappedByteBuffer opened = file.map(FileChannel.MapMode.READ_ONLY, 0, file.size());
-        opened.position((int) headerSize);
-        return opened;
     }
 }
